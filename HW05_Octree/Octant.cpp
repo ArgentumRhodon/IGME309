080420200517a18e4e919d23cb4a1a837034794d
Lucas Corey <rit.lucascorey@gmail.com>
2023-04-29 18:29:59 -0400

    Completed HW5-Octree

--- HW05_Octree/Octant.cpp
+++ HW05_Octree/Octant.cpp
@@ -1,4 +1,5 @@
 #include "Octant.h"
+#include <stack>
 using namespace BTX;
 //  Octant
 uint Octant::m_uOctantCount = 0;
@@ -19,24 +20,65 @@ Octant::Octant(uint a_nMaxLevel, uint a_nIdealEntityCount)
 	m_pRoot = this;
 	m_lChild.clear();
 
-	//create a rigid body that encloses all the objects in this octant, it necessary you will need
-	//to subdivide the octant based on how many objects are in it already an how many you IDEALLY
-	//want in it, remember each subdivision will create 8 children for this octant but not all children
-	//of those children will have children of their own
+	// Create a rigid body that encloses all the objects in this octant, it necessary you will need
+	// to subdivide the octant based on how many objects are in it already an how many you IDEALLY
+	// want in it, remember each subdivision will create 8 children for this octant but not all children
+	// of those children will have children of their own
 
-	//The following is a made-up size, you need to make sure it is measuring all the object boxes in the world
 	std::vector<vector3> lMinMax;
-	lMinMax.push_back(vector3(-50.0f));
-	lMinMax.push_back(vector3(25.0f));
+
+	// Min and max positions of entity coordinates
+	m_v3Min = m_pEntityMngr->GetRigidBody(0)->GetMinGlobal();
+	m_v3Max = m_pEntityMngr->GetRigidBody(0)->GetMaxGlobal();
+
+	// Through the mins and maxes of each object, find the absolute minimum and absolute maximum
+	// square space in which all objects live in.
+	for (int i = 0; i < m_pEntityMngr->GetEntityCount(); i++)
+	{
+		// Minimum and maximum vertices of the current object's rigid body
+		vector3 entityMin = m_pEntityMngr->GetRigidBody(i)->GetMinGlobal();
+		vector3 entityMax = m_pEntityMngr->GetRigidBody(i)->GetMaxGlobal();
+
+		if (m_v3Max.x < entityMax.x) m_v3Max.x = entityMax.x;
+		else if (m_v3Min.x > entityMin.x) m_v3Min.x = entityMin.x;
+
+		if (m_v3Max.y < entityMax.y) m_v3Max.y = entityMax.y;
+		else if (m_v3Min.y > entityMin.y) m_v3Min.y = entityMin.y;
+
+		if (m_v3Max.z < entityMax.z) m_v3Max.z = entityMax.z;
+		else if (m_v3Min.z > entityMin.z) m_v3Min.z = entityMin.z;
+	}
+
+	// Make the rigid body square by finding the longest side length
+	float longestSideLength = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		if (abs(m_v3Min[i]) > longestSideLength)
+		{
+			longestSideLength = abs(m_v3Min[i]);
+		}
+		if (abs(m_v3Max[i]) > longestSideLength)
+		{
+			longestSideLength = abs(m_v3Max[i]);
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		m_v3Min[i] = -longestSideLength;
+		m_v3Max[i] = longestSideLength;
+	}
+
+	// Create rigid body for root
+	lMinMax.push_back(m_v3Min);
+	lMinMax.push_back(m_v3Max);
 	RigidBody pRigidBody = RigidBody(lMinMax);
 
-
-	//The following will set up the values of the octant, make sure the are right, the rigid body at start
+	//The following will set up the values of the octant, make sure they are right, the rigid body at start
 	//is NOT fine, it has made-up values
 	m_fSize = pRigidBody.GetHalfWidth().x * 2.0f;
 	m_v3Center = pRigidBody.GetCenterLocal();
-	m_v3Min = m_v3Center - pRigidBody.GetHalfWidth();
-	m_v3Max = m_v3Center + pRigidBody.GetHalfWidth();
+	/*m_v3Min = m_v3Center - pRigidBody.GetHalfWidth();
+	m_v3Max = m_v3Center + pRigidBody.GetHalfWidth();*/
 
 	m_uOctantCount++; //When we add an octant we increment the count
 	ConstructTree(m_uMaxLevel); //Construct the children
@@ -48,11 +90,47 @@ bool Octant::IsColliding(uint a_uRBIndex)
 	//If the index given is larger than the number of elements in the bounding object there is no collision
 	//As the Octree will never rotate or scale this collision is as easy as an Axis Alligned Bounding Box
 	//Get all vectors in global space (the octant ones are already in Global)
+	if (a_uRBIndex >= m_pEntityMngr->GetEntityCount()) // ???
+	{
+		return false;
+	}
+
+	RigidBody* entityBody = m_pEntityMngr->GetRigidBody(a_uRBIndex);
+
+	// AABB collision check
+	if (m_v3Max.x < entityBody->GetMinGlobal().x)
+		return false;
+	if (m_v3Min.x > entityBody->GetMaxGlobal().x)
+		return false;
+
+	if (m_v3Max.y < entityBody->GetMinGlobal().y)
+		return false;
+	if (m_v3Min.y > entityBody->GetMaxGlobal().y)
+		return false;
+
+	if (m_v3Max.z < entityBody->GetMinGlobal().z)
+		return false;
+	if (m_v3Min.z > entityBody->GetMaxGlobal().z)
+		return false;
+
 	return true; // for the sake of startup code
 }
 void Octant::Display(uint a_nIndex, vector3 a_v3Color)
 {
 	// Display the specified octant
+	// Search through the tree until we find the one with the specified ID
+	if (a_nIndex == m_uID)
+	{
+		m_pModelMngr->AddWireCubeToRenderList(glm::translate(IDENTITY_M4, m_v3Center) *
+			glm::scale(vector3(m_fSize)), a_v3Color);
+	}
+	else if(m_uChildren != 0)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_pChild[i]->Display(a_nIndex, a_v3Color);
+		}
+	}
 }
 void Octant::Display(vector3 a_v3Color)
 {
@@ -60,6 +138,18 @@ void Octant::Display(vector3 a_v3Color)
 	//even if other objects are created
 	m_pModelMngr->AddWireCubeToRenderList(glm::translate(IDENTITY_M4, m_v3Center) *
 		glm::scale(vector3(m_fSize)), a_v3Color);
+
+	// Guard clause against empty children list
+	if (m_uChildren == 0)
+	{
+		return;
+	}
+
+	// Display children
+	for (int i = 0; i < 8; i++)
+	{
+		m_pChild[i]->Display(a_v3Color);
+	}
 }
 void Octant::Subdivide(void)
 {
@@ -68,22 +158,89 @@ void Octant::Subdivide(void)
 		return;
 
 	//If this node has been already subdivided return without changes
-	if (m_uChildren != 0)
-		return;
 
 	//Subdivide the space and allocate 8 children
+	float quarterWidth = m_fSize / 4;
+	float halfWidth = m_fSize / 2;
+
+	m_pChild[0] = new Octant(m_v3Center + vector3(quarterWidth), halfWidth);
+	m_pChild[1] = new Octant(m_v3Center + vector3(-quarterWidth, quarterWidth, quarterWidth), halfWidth);
+	m_pChild[2] = new Octant(m_v3Center + vector3(quarterWidth, -quarterWidth, quarterWidth), halfWidth);
+	m_pChild[3] = new Octant(m_v3Center + vector3(quarterWidth, quarterWidth, -quarterWidth), halfWidth);
+
+	m_pChild[4] = new Octant(m_v3Center + vector3(-quarterWidth, -quarterWidth, quarterWidth), halfWidth);
+	m_pChild[5] = new Octant(m_v3Center + vector3(-quarterWidth, quarterWidth, -quarterWidth), halfWidth);
+	m_pChild[6] = new Octant(m_v3Center + vector3(quarterWidth, -quarterWidth, -quarterWidth), halfWidth);
+	m_pChild[7] = new Octant(m_v3Center + vector3(-quarterWidth), halfWidth);
+
+	m_uChildren = 8;
+
+	// Recursive subdivision
+	for (int i = 0; i < 8; i++)
+	{
+		// Set the levels for the created children
+		m_pChild[i]->m_uLevel = m_uLevel + 1;
+
+		if (m_pChild[i]->ContainsAtLeast(m_uIdealEntityCount))
+		{
+			m_pChild[i]->Subdivide();
+		}
+	}
 }
 bool Octant::ContainsAtLeast(uint a_nEntities)
 {
 	//You need to check how many entity objects live within this octant
-	return false; //return something for the sake of start up code
+
+	// If this isn't a leaf node, return false
+	if (m_uChildren > 0)
+	{
+		return false;
+	}
+
+	uint numEntitesContained = 0;
+	for (int i = 0; i < m_pEntityMngr->GetEntityCount(); i++)
+	{
+		// If colliding with this octant, and thus contained within, increase count
+		if (IsColliding(i))
+		{
+			numEntitesContained++;
+		}
+		// If min number found, return
+		if (numEntitesContained >= a_nEntities)
+		{
+			return true;
+		}
+	}
+
+	// The minimum number of entities was not found to be contained
+	return false;
 }
 void Octant::AssignIDtoEntity(void)
 {
 	//Recursive method
 	//Have to traverse the tree and make sure to tell the entity manager
 	//what octant (space) each object is at
-	m_pEntityMngr->AddDimension(0, m_uID);//example only, take the first entity and tell it its on this space
+
+	// Traverse the tree
+	if (m_uChildren != 0)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_pChild[i]->AssignIDtoEntity();
+		}
+	}
+	else
+	{
+		// Check for enitities within this node
+		for (int i = 0; i < m_pEntityMngr->GetEntityCount(); i++)
+		{
+			// If colliding with this octant, and thus contained within, add id to entity
+			if (IsColliding(i))
+			{
+				m_pEntityMngr->AddDimension(i, m_uID);
+			}
+		}
+	}
 }
 //-------------------------------------------------------------------------------------------------------------------
 // You can assume the following is fine and does not need changes, you may add onto it but the code is fine as is
